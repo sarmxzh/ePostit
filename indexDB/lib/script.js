7d64fd224c27f841da20591049486aabea79882f
Jungwoo Park <Jungwoo@Jungwoos-Air.andover.edu>
2018-02-22 09:05:35 -0500

    Some code that uses in indexDB to store created post its

---
+++ indexDB/lib/script.js
@@ -0,0 +1,28 @@
+const DB = new MemoDB();
+
+async function addRandom(emailId) {
+
+    let newMemo = "" + Math.floor(Math.random() * 10);
+
+    let item = await DB.retrieve(emailId);
+    if (typeof item === 'undefined') {
+        item = {emailId: emailId, memos: []};
+    }
+    console.log(item); // {emailId: 'ABC', memos: ['sdfds','sdfsd','sdfsdf']}
+
+    item.memos.push(newMemo);
+    DB.store(emailId, item);
+
+    updatePage(item.memos);
+}
+
+function updatePage(memos) {
+    let list = document.querySelector("#list");
+    list.innerHTML = "";
+
+    memos.forEach(memo => {
+        let div = document.createElement("div");
+        div.innerText = memo;
+        list.appendChild(div);
+    })
+}
